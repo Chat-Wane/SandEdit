c1d45491a95cc4dc6fe739fb16f71f4a9dd6502c
Chat-Wane <brice.nedelec@gmail.com>
2014-05-05 18:35:44 +0200

    aeneighbours

--- config.js
+++ config.js
@@ -1,21 +1,21 @@
-module.exports.dataFolder = "./public/bn";
-module.exports.ipFile = "./sandedit/IPs";
+module.exports.dataFolder = "./data/";
+module.exports.ipFile = "./IPs";
 
 module.exports.STARTTIME = (1*60000); // 1 min
-module.exports.RUNNINGTIME = (50*60000); // 5 min
-module.exports.STOPTIME = (5*60000); // 1 min
-module.exports.PARTITIONTIME = 50;
+module.exports.RUNNINGTIME = (60*60000); // 5 min
+module.exports.STOPTIME = (1*60000); // 1 min
+module.exports.PARTITIONTIME = 60;
 
 module.exports.PEERS = 2; // 5 peers per cluster
-module.exports.NEIGHBOURS = 9;
-module.exports.OPERATIONS = 10000; // 5k insertions per cluster
-module.exports.HOP = 4;
+module.exports.NEIGHBOURS = 1;
+module.exports.OPERATIONS = 600000; // 5k insertions per cluster
 module.exports.MBSPCALL = 100; // membershipcall number
 				    // before changin nwrk
 module.exports.ANTIENTROPY = 8000; // 8s
+module.exports.AENEIGHBOURS = 1;
 
-module.exports.SUBNET = "172.16.112.99";
-module.exports.MASK = "20";
+module.exports.SUBNET = "127.0.0.1";
+module.exports.MASK = "255.0.0.0";
 module.exports.PORT= 1400;
 
 module.exports.CHECKPOINTS =[100, 500, 1000, 5000, 10000, 20000, 30000, 40000,
